59e99ea1cdfd110f658a34c961a218ec53f9d3b7
Hooooni98 <piel07@naver.com>
2022-11-04 19:02:14 +0900

    [FOOIY-7] 디렉토리 구조 설정

--- fooiy/App.js
+++ fooiy/App.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 
 const App = () => {
   return <View></View>;
