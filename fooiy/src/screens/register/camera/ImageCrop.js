95d9fa9a87d1c21137db1260292f65a309da037c
Hooooni98 <piel07@naver.com>
2022-11-13 20:55:29 +0900

    [FOOIY-19] 커스텀 스택 헤더 세팅

--- fooiy/src/screens/register/camera/ImageCrop.js
+++ fooiy/src/screens/register/camera/ImageCrop.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Image, View, Text, Dimensions} from 'react-native';
+import {StackHeader} from '../../../common_ui/headers/StackHeader';
 
 const ImageCrop = props => {
   const photo = props.route.params.photo;
   const width = Dimensions.get('window').width;
   return (
     <View style={{flex: 1}}>
+      <StackHeader title="사진등록" />
       <Image
         source={{uri: photo}}
         style={{
