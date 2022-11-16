a00d3cf0cba58d035305a3eedb966de3ce9274e3
gbtwld <gbtwld@gmail.com>
2022-11-16 19:33:52 +0900

    [FOOIY-14] map으로 marker 여러개 찍기

--- fooiy/src/screens/map/Map.js
+++ fooiy/src/screens/map/Map.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  ScrollView,
-  SafeAreaView,
-} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
 import NaverMap from './NaverMap';
 
--- fooiy/src/screens/map/NaverMap.js
+++ fooiy/src/screens/map/NaverMap.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import {Text} from 'react-native';
 import NaverMapView, {Align, Marker} from 'react-native-nmap';
+import MarkerArr from './MarkerArr';
 
 const NaverMap = () => {
-  const currentLocation = {latitude: 37.296425, longitude: 126.838933};
+  const currentLocation = {latitude: 37.301796, longitude: 126.840034};
   const markerImg = '../../../assets/icons/marker/marker.png';
-  const markerClickedImg = '../../../assets/icons/marker/marker_clicked.png';
-
-  const onClickMarker = e => {
-    console.dir(e);
-  };
 
   return (
     <NaverMapView
-      style={{width: '100%', height: '100%'}}
+      style={{flex: 1}}
       showsMyLocationButton={true}
       center={{...currentLocation, zoom: 16}}
-      onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-      onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-      onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
-      <Marker
-        coordinate={currentLocation}
-        width={48}
-        height={58}
-        image={require(markerImg)}
-        caption={{
-          text: '98%',
-          align: Align.Center,
-          color: '#FE5B5C',
-          textSize: 13,
-        }}
-        // 글자 위치 변경 안돼서 subCaption 넣어서 위로 밀었음
-        subCaption={{text: 'ㅤ', textSize: 15}}
-        onClick={e => onClickMarker(e)}
-      />
+      // onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+      // onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+      // onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
+    >
+      {MarkerArr.map((elem, index) => (
+        <Marker
+          key={index}
+          coordinate={elem.Location}
+          width={48}
+          height={58}
+          image={require(markerImg)}
+          // image={require(markerImg)}
+          caption={{
+            text: `${elem.suitability}%`,
+            align: Align.Center,
+            color: '#FE5B5C',
+            textSize: 13,
+          }}
+          // 글자 위치 변경 안돼서 subCaption 넣어서 위로 밀었음
+          subCaption={{text: 'ㅤ', textSize: 15}}
+        />
+      ))}
     </NaverMapView>
   );
 };
