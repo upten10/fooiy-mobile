cedcdf276222c0f56d1dbd418743d14e24e30b58
Hooooni98 <piel07@naver.com>
2022-11-20 16:08:55 +0900

    [FOOIY-33] 개척/기록, 카메라 줌인 | 바텀 네비 제거 | 스택 헤더 생성

--- fooiy/src/screens/register/camera/ImageCrop.js
+++ fooiy/src/screens/register/camera/ImageCrop.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import {Image, View, Dimensions} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {Image, View, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {CropView} from 'react-native-image-crop-tools';
+
 import {StackHeader} from '../../../common_ui/headers/StackHeader';
+import {globalVariable} from '../../../common/globalVariable';
 
 const ImageCrop = props => {
   const navigation = useNavigation();
-  const photo = props.route.params.photo;
-  const width = Dimensions.get('window').width;
+  const [cropPhoto, setCropPhoto] = useState(false);
+  const cropViewRef = useRef(null);
+  const [photo, setPhoto] = useState(props.route.params.photo);
+  const width = globalVariable.width;
   const go_next = () => {
     navigation.navigate('TypingContent', {photo_list: [photo]});
   };
@@ -14,13 +19,49 @@ const ImageCrop = props => {
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
       <StackHeader title="사진등록" next={go_next} />
-      <Image
-        source={{uri: photo}}
-        style={{
-          width: width,
-          height: width,
-        }}
-      />
+      {cropPhoto ? (
+        <View>
+          <CropView
+            ref={cropViewRef}
+            sourceUrl={photo}
+            style={{
+              width: width,
+              height: width,
+            }}
+            onImageCrop={res => {
+              setPhoto(res.uri);
+              setCropPhoto(false);
+            }}
+            keepAspectRatio
+            aspectRatio={{width: 9, height: 9}}
+          />
+          <View style={{height: width / 10, flexDirection: 'row'}}>
+            <Button
+              title="저장"
+              onPress={() => cropViewRef.current.saveImage(true)}
+            />
+            <Button
+              title="우회전"
+              onPress={() => cropViewRef.current.rotateImage(true)}
+            />
+            <Button
+              title="좌회전"
+              onPress={() => cropViewRef.current.rotateImage(false)}
+            />
+          </View>
+        </View>
+      ) : (
+        <View>
+          <Image
+            source={{uri: photo}}
+            style={{
+              width: width,
+              height: width,
+            }}
+          />
+          <Button title="편집" onPress={() => setCropPhoto(true)} />
+        </View>
+      )}
     </View>
   );
 };
