5f18726aaa6ce5ae039bf2b09851ace9dd2db3b6
gbtwld <gbtwld@gmail.com>
2022-11-25 15:23:20 +0900

    [FOOIY-36] 지도 현위치 버튼

--- fooiy/src/screens/map/NaverMap.js
+++ fooiy/src/screens/map/NaverMap.js
@@ -1,52 +1,110 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import NaverMapView, {Align, Marker} from 'react-native-nmap';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import {globalVariable} from '../../common/globalVariable';
+import Location from './Location';
 import MarkerArr from './MarkerArr';
 
-const NaverMap = () => {
-  const currentLocation = {latitude: 37.301796, longitude: 126.840034};
-  const markerImg = '../../../assets/icons/marker/marker.png';
-  const markerClickedImg = '../../../assets/icons/marker/marker_clicked.png';
+const markerImg = '../../../assets/icons/marker/marker.png';
+const markerClickedImg = '../../../assets/icons/marker/marker_clicked.png';
+
+const NaverMap = props => {
+  const mapView = useRef(null);
+  let curLocation = Location();
 
   const [clickedIndex, setClickedIndex] = useState(null);
 
-  const handleClick = index => {
+  const onClickMarker = index => {
     setClickedIndex(index);
   };
 
+  const onClickLocationBtn = () => {
+    mapView.current.setLocationTrackingMode(3);
+  };
+
+  const LocationBtn = () => {
+    return (
+      <Pressable style={styles.button} onPress={onClickLocationBtn}>
+        <Text style={styles.text}>{'현위치'}</Text>
+      </Pressable>
+    );
+  };
+
   return (
-    <NaverMapView
-      style={{flex: 1}}
-      showsMyLocationButton={true}
-      center={{...currentLocation, zoom: 16}}
-      onMapClick={() => setClickedIndex(null)}
-      // onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-      onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-      // onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
-    >
-      {MarkerArr.map((elem, index) => {
-        return (
-          <Marker
-            key={index}
-            coordinate={elem.Location}
-            width={40}
-            height={50}
-            image={
-              clickedIndex === index
-                ? require(markerClickedImg)
-                : require(markerImg)
-            }
-            caption={{
-              text: `${elem.suitability}%`,
-              align: Align.Center,
-              color: '#FE5B5C',
-              textSize: 13,
-            }}
-            onClick={() => handleClick(index)}
-          />
-        );
-      })}
-    </NaverMapView>
+    <View>
+      <NaverMapView
+        ref={mapView}
+        style={styles.map}
+        center={{...curLocation, zoom: 16}}
+        onMapClick={() => setClickedIndex(null)}
+        // onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+        // onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+        // onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
+      >
+        {MarkerArr.map((elem, index) => {
+          return (
+            <Marker
+              key={index}
+              coordinate={elem.Location}
+              width={40}
+              height={50}
+              image={
+                clickedIndex === index
+                  ? require(markerClickedImg)
+                  : require(markerImg)
+              }
+              caption={{
+                text: `${elem.suitability}%`,
+                align: Align.Center,
+                color: '#FE5B5C',
+                textSize: 13,
+              }}
+              onClick={() => onClickMarker(index)}
+            />
+          );
+        })}
+      </NaverMapView>
+      <LocationBtn />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    width: globalVariable.width,
+    height: globalVariable.height,
+  },
+  button: {
+    position: 'absolute',
+    bottom: globalVariable.height / 5,
+    left: globalVariable.width / 1.25,
+    backgroundColor: 'white',
+    borderRadius: 6,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 0.3, // 낮을수록 진해짐
+        },
+        shadowOpacity: 0.15, // 높을수록 진해짐
+        shadowRadius: 5,
+      },
+      android: {
+        elevation: 6,
+      },
+    }),
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#FE5B5C',
+  },
+});
 
 export default NaverMap;
