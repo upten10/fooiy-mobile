9e472760e1d1c6d46a8764c8e7549e6eae6bb767
Hooooni98 <piel07@naver.com>
2022-11-28 21:10:43 +0900

    [FOOIY-42] 지도, 마커 API

--- fooiy/src/common/Enums.js
+++ fooiy/src/common/Enums.js
@@ -4,4 +4,5 @@ export const apiUrl = {
   FEED_LIST: 'feeds/feed/',
   SHOP_LIST: 'feeds/feed/shop/',
   MAP_SHOP_LIST: 'shops/shop/shop_map_list/',
+  MAP_SHOP_MARKER: 'shops/shop/shop_map_picker/',
 };
