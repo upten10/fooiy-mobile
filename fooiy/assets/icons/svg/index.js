7f77bb042338a1b46b65e0f8cbb5a5ab54f35b83
Hooooni98 <piel07@naver.com>
2022-12-06 17:02:09 +0900

    [FOOIY-45] 피드, 포크 보관 공유 icon 등록

---
+++ fooiy/assets/icons/svg/index.js
@@ -0,0 +1,6 @@
+export {default as Fork} from './ic_fork.svg';
+export {default as ForkFocused} from './ic_fork_focused.svg';
+export {default as Store} from './ic_store.svg';
+export {default as StoreFocused} from './ic_store_focused.svg';
+export {default as Share} from './ic_share.svg';
+export {default as Comment} from './ic_comment.svg';
