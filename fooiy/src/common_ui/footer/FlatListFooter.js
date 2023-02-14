7da9318f47f20afdfd1f25fef80edb56acec3053
Hooooni98 <piel07@naver.com>
2023-02-14 11:48:03 +0900

    QA 수정 2

--- fooiy/src/common_ui/footer/FlatListFooter.js
+++ fooiy/src/common_ui/footer/FlatListFooter.js
@@ -2,8 +2,23 @@ import React from 'react';
 import {View} from 'react-native';
 import {globalVariable} from '../../common/globalVariable';
 
-const FlatListFooter = () => {
-  return <View style={{height: globalVariable.tabBarHeight}}></View>;
+const FlatListFooter = props => {
+  const {h} = props;
+  if (props === undefined) {
+    return (
+      <View
+        style={{
+          height: globalVariable.tabBarHeight,
+        }}></View>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          height: h,
+        }}></View>
+    );
+  }
 };
 
 export default FlatListFooter;
