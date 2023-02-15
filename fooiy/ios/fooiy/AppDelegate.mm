c1131a7805f43de7b2ad24965e491ba2abcb6bd7
gbtwld <gbtwld@gmail.com>
2023-02-15 14:07:52 +0900

    [IOS] CodePush Setting

--- fooiy/ios/fooiy/AppDelegate.mm
+++ fooiy/ios/fooiy/AppDelegate.mm
@@ -18,6 +18,7 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 
 #import <react/config/ReactNativeConfig.h>
+#import <CodePush/CodePush.h>
 
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
@@ -92,7 +93,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
